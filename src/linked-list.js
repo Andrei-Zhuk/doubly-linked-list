3b5f31effcadbb1b8cfa419a34b882617ba0b27a
Andrei-Zhuk <zhuk_andrey@tut.by>
2017-02-02 13:40:17 +0300

    doubly-linled-list: make code shorter

--- src/linked-list.js
+++ src/linked-list.js
@@ -10,7 +10,7 @@ class LinkedList {
 
     append(data) {
         var node = new Node(data)
-        if (this.length == 0) {
+        if (this.length === 0) {
             this._head = node;
             this._tail = node;
         } else {
@@ -40,58 +40,46 @@ class LinkedList {
     }
 
     at(index) {
-        if (index > -1 && index < this.length){
-            var current = this._head,
-                i = 0;
+        
+        var current = this._head,
+            i = 0;
 
-            while(i++ < index){
-                current = current.next;
-            }
-
-            return current.data;
-        } else {
-            return null;
+        while(i++ < index){
+            current = current.next;
         }
+
+        return current.data;
     }
 
     insertAt(index, data) {
         var node = new Node(data)
-        if (index > -1 && index < this.length+1) {
-
-            var current = this._head,
-                i = 0;
-
-            if (index === 0) {
-
-                if (this.length == 0) {
-                    this._head = node;
-                    this._tail = node;
-                } else {
-                this._head.prev = node;
-                node.next = this._head;
+        var current = this._head,
+            i = 0;
+        if (index === 0) {
+            if (this.length == 0) {
                 this._head = node;
-                }
-
-            } else if (index === this.length){
-                this._tail.next = node;
-                node.prev = this._tail;
                 this._tail = node;
             } else {
-                while(i++ < index){
-                    current = current.next;
-                }
-                node.next = current;
-                node.prev = current.prev;
-                current.prev.next = node;
-                current.prev = node;
-                
+            this._head.prev = node;
+            node.next = this._head;
+            this._head = node;
             }
 
-            this.length++;
-            return this;            
+        } else if (index === this.length){
+            this._tail.next = node;
+            node.prev = this._tail;
+            this._tail = node;
         } else {
-            return null;
+            while(i++ < index){
+                current = current.next;
+            }
+            node.next = current;
+            node.prev = current.prev;
+            current.prev.next = node;
+            current.prev = node;    
         }
+        this.length++;
+        return this;            
     }
 
     isEmpty() {
@@ -110,35 +98,28 @@ class LinkedList {
     }
 
     deleteAt(index) {
-        if (index > -1 && index < this.length){
-
-            var current = this._head;
-            var i = 0;
-
-            if (index === 0){
-                this._head = current.next;
-                if (this.length == 1){
-                    this._tail = null;
-                } else {
-                    this._head.prev = null;
-                }
-
-            } else if (index === this.length - 1) {
-                current = this._tail;
-                this._tail = current.prev;
-                this._tail.next = null;
+        var current = this._head;
+        var i = 0;
+        if (index === 0){
+            this._head = current.next;
+            if (this.length == 1){
+                this._tail = null;
             } else {
-                while(i++ < index){
-                    current = current.next;
-                }
-                current.prev.next = current.next;
-                current.next.prev = current.prev;
-            };
-            this.length--;
-            return this;       
+                this._head.prev = null;
+            }
+        } else if (index === this.length - 1) {
+            current = this._tail;
+            this._tail = current.prev;
+            this._tail.next = null;
         } else {
-            return null;
-        }
+            while(i++ < index){
+                current = current.next;
+            }
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+        };
+        this.length--;
+        return this;       
     }
 
     reverse() {
@@ -194,14 +175,5 @@ class LinkedList {
         return -1;
     }
 }
-// var l = new LinkedList()
-// l.append(1)
-// l.append(2)
-// l.append(3)
-// l.append(4)
-// alert(l.reverse())
-// alert(l.at(0))
-// alert(l.at(1))
-// alert(l.at(2))
-// alert(l.at(3))
+
 module.exports = LinkedList;
